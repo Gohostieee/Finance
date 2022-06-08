32877084fb2b1111433104892fac325ae8bae0e2
Gohost <94026225+Gohostieee@users.noreply.github.com>
2022-06-07 23:43:40 -0400

    Removed some useless lines

--- pyServer/main.py
+++ pyServer/main.py
@@ -15,13 +15,8 @@ print(dbKeys)
 connection = mysql.connector.connect(user=dbKeys["user"],password=dbKeys["password"],host=dbKeys["host"],database=dbKeys["dbName"])
 
 cursor = connection.cursor()
-#cursor.execute("select date from coinPriceData where date in ('2022-05-01') ")
-#print(cursor.fetchall())
-#input()
+
 app = Flask(__name__)
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
 
 @app.route('/api', methods=['GET', 'POST'])
 def apiRequest():
@@ -40,28 +35,9 @@ def apiRequest():
                 print(cursor.execute(queryString[2],queryString[1]+queryString[0]))
                 return {'response':cursor.fetchall()}
             case "lowtohigh":
-                """queryString = getNameDatePriceQuery(request)
-                print(cursor.execute(queryString[2],queryString[1]+queryString[0]))
-                priceHighLow = [[x for x in cursor.fetchall()],[]]
-                currDate = [[priceHighLow[0][0][2],priceHighLow[0][0][2]],'',0,0]
-                currDate[1]=priceHighLow[0][0][1]
-                for x in priceHighLow[0]:
-                    if x[1] != currDate[1]:
-                        priceHighLow[1].append([x[0],currDate[1],[currDate[0][0], currDate[0][1]]])
-                        print(currDate, "what")
-                        currDate[0][0]=x[2]
-                        currDate[0][1]=x[2]
-                        currDate[1]=x[1]
-                    if x[2]<currDate[0][0]:
-                        print(currDate,x)
-
-                        currDate[0][0]=x[2]
-                    elif x[2]>currDate[0][1]:
-                        currDate[0][1]=x[2]
-"""
                 return extractDatePrice(request,cursor)
             case default:
-                return {'response':'RON you moronic new nigga'}
+                return {'response':'500 server error'}
     else:
         print("failed")
 
